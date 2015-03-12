7e15c88a675ef6f52bec3619a86d93712ebb3f7f
Chamindu R. Munasinghe <chamindu@calcey.com>
2015-03-12 16:10:04 +0530

    Adding unit tests.

--- spec/filesystem_spec.rb
+++ spec/filesystem_spec.rb
@@ -5,10 +5,29 @@ describe CloudFS::FileSystem do
    session = CloudFS::Session.new(Configuration::CLIENT_ID, Configuration::SECRET, Configuration::HOST)
    session.authenticate(Configuration::USERNAME, Configuration::PASSWORD)
    @subject = session.filesystem 
+   @test_folder = @subject.root.create_folder(Configuration::TEST_FOLDER, exists: 'OVERWRITE')
  end
 
  it "Should have a root" do
    @subject.root.type.must_equal 'folder'
    @subject.root.name.must_equal 'root'
  end
+
+ describe "Listing items" do
+  before do
+   @list_folder = @test_folder.create_folder('list_test', exists: 'OVERWRITE')
+  end
+
+  after do
+    @list_folder.delete
+  end
+
+  it "#list" do
+    items = @subject.list(item: @test_folder)
+    items.must_be_instance_of Array 
+    items.length.must_equal 1
+  end
+ end
+
+
 end
