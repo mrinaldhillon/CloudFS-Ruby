a77c2752480f8d3f28b6e3493b9ce5a8c67c3257
Gayan De Silva <gayan.d@calcey.com>
2015-03-21 17:52:54 +0530

    Configuration changes

--- spec/configuration.rb
+++ spec/configuration.rb
@@ -1,8 +1,8 @@
 module Configuration
-  CLIENT_ID = '6VaRUN0AJDftZaaFyQy98oHvVmuUjI8fJz6UHIkQct0'
-  SECRET = 'fSXNM3HhRJaNM-N8gJsADjYwxvQnCMyZEh95BQpjuNRpt2j5EGVInd8UtTbmjg8dtd1qK0sb1NDmN7ClxxdanA'
-  HOST = 'evyg9ym7w1.cloudfs.io'
-  USERNAME = 'dhanushka@calcey.com'
-  PASSWORD = 'dhanushka'
+  CLIENT_ID = '<add client id here>'
+  SECRET = '<add client secret here>'
+  HOST = '<add cloudfs endpoint here>'
+  USERNAME = '<add cloudfs username here>'
+  PASSWORD = '<add cloudfs password here>'
   TEST_FOLDER = 'ruby_sdk_test'
 end
